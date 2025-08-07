f8de5b3e4583fd78587c09555512347d381a7e83
24ftt1810 <24ftt1810@student.pb.edu.bn>
2025-08-07 19:07:45 +0800

    Add files via upload

---
+++ Fundamental1/Fundamental1.java
@@ -0,0 +1,48 @@
+public class Fundamental1 {
+        public static void main(String[] args){
+    System.out.println("Welcome to AD2204 Introduction to Programming");
+    System.out.println("This is paractical 2");
+         
+    System.out.println("(\\__/) Iam an");
+    System.out.println("( @w@)       OOO  W        W L");
+    System.out.println("/) W )      O    O  W  W  W  L");
+    System.out.println("  ^ ^        OOO      W W    L L L");
+
+System.out.println("a       a^2       a^3\n" + "1       1       1\n"+"2       4       8\n"+"3       9       27\n"+"4       16      64\n");
+
+System.out.println("The result of  7.5 x 2.5 - 1.5 x 3 ");
+System.out.println("              ---------------------    is 9.5");
+System.out.println("                     4-2.5");
+
+
+System.out.println("             .=\" \"=.");
+System.out.println("           /        \\");
+System.out.println("          |  O    O  |");
+System.out.println("          \\    /\\    /");
+System.out.println("          /\'-= \\/=-\' \\      CHIP CHIP !!");
+System.out.println("         /  /      \\  \\");
+System.out.println("         \\ /\\      /\\ /     ");
+System.out.println("             '.  .'");
+System.out.println("             /|\\/|\\");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ }
+}
